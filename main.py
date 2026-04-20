d20dcf6413e248718727abee4e3ad1b0f157f8c3
chocolatito <dannny.mx@gmail.com>
2026-04-20 20:30:49 -0300

    import argparse

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import argparse
+import json
 from src.crawler import Crawler
 
-movie_to_look_for = [
-    {"title": "primal fear", "year": 1996},
-    {"title": "Raging Bull", "year": 1980},
-]
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--json_input", required=True)
+
+    args = parser.parse_args()
+    try:
+        with open(args.json_input, "r", encoding="utf-8") as file:
+            movie_to_look_for = json.load(file)
+    except Exception as e:
+        raise e
     crawler_object = Crawler(movie_to_look_for)
     crawler_object.main()
